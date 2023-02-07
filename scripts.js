a6c9c5e743b14cf7466c3e8191522623acf77dad
CandersA <123170339+CandersA@users.noreply.github.com>
2023-02-07 15:38:59 +0200

    Change how code works completely from duplicating book to creating new

--- scripts.js
+++ scripts.js
@@ -3,18 +3,17 @@ const submitBookForm = document.getElementById('submitform');
 const bookForm = document.getElementById('bookadd');
 const darken = document.getElementById('darken');
 const closeBtn = document.querySelector('.closebtn');
-const singleBook = document.querySelector('.book');
-const removeBtn = document.querySelector('.remove');
-
+const bookSection = document.getElementById('main');
+const emptyPara = document.getElementById('empty');
 let i = 0;
-
 const myLibrary = [];
 
-function Book(name, author, numOfPages, read) {
+function Book(name, author, numOfPages, read, bookNr) {
   this.name = name;
   this.author = author;
   this.numOfPages = numOfPages;
   this.read = read;
+  this.bookNr = bookNr;
 }
 
 function openForm() {
@@ -32,23 +31,55 @@ function getData() {
   const bookAuthor = document.getElementById('bookauthor').value;
   const pageNumber = document.getElementById('numofpages').value;
   const isRead = document.getElementById('bookread').checked;
-  myLibrary[i] = new Book(bookName, bookAuthor, pageNumber, isRead);
+  myLibrary[i] = new Book(bookName, bookAuthor, pageNumber, isRead, i);
   closeForm();
+}
+
+function removeBook() {
+  const allBooks = Array.from(document.getElementsByClassName('book'));
+  if (allBooks.length === 1) {
+    emptyPara.style.display = 'block';
+  }
+  const removeBtn = this.querySelector('.remove');
+  const parentOfButton = removeBtn.parentNode;
+  parentOfButton.remove();
+}
+
+function createBook() {
+  emptyPara.style.display = 'none';
+  const newBook = document.createElement('div');
+  const bookName = document.createElement('span');
+  const bookAuthor = document.createElement('span');
+  const bookPages = document.createElement('span');
+  const readBtn = document.createElement('button');
+  const removeBtn = document.createElement('button');
+  readBtn.classList.add('read');
+  removeBtn.classList.add('remove');
+  newBook.classList.add('book');
+  newBook.appendChild(bookName);
+  newBook.appendChild(bookAuthor);
+  newBook.appendChild(bookPages);
+  newBook.appendChild(readBtn);
+  newBook.appendChild(removeBtn);
+  bookName.textContent = myLibrary[i].name;
+  bookAuthor.textContent = myLibrary[i].author;
+  bookPages.textContent = `${myLibrary[i].numOfPages} pages`;
+  removeBtn.textContent = 'Remove';
+  if (myLibrary[i].read === true) {
+    readBtn.textContent = 'Read';
+    readBtn.classList.add('green');
+  } else {
+    readBtn.textContent = 'Not read';
+  }
+  bookSection.appendChild(newBook);
+  removeBtn.addEventListener('click', () => {
+    removeBook.apply(newBook);
+  }, { once: true });
 }
 
 function displayBook() {
   getData();
-  const newBook = singleBook.cloneNode(true);
-  const spanArray = singleBook.getElementsByTagName('span');
-  spanArray[0].textContent = myLibrary[i].name;
-  spanArray[1].textContent = myLibrary[i].author;
-  spanArray[2].textContent = `${myLibrary[i].numOfPages} pages`;
-  const buttons = singleBook.getElementsByTagName('button');
-  if (myLibrary[i].read === true) {
-    buttons[0].textContent = 'Read';
-    buttons[0].style.backgroundColor = '#9be06c';
-  }
-  singleBook.after(newBook);
+  createBook();
   i += 1;
 }
 
