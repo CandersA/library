babcd34d9300bf2030c878048bffdd534175cca9
CandersA <123170339+CandersA@users.noreply.github.com>
2023-02-07 11:32:30 +0200

    Add form popup when add book button is clicked

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-console.log('js');
+const addBookBtn = document.querySelector('.addbook');
+const bookForm = document.getElementById('bookadd');
+const darken = document.getElementById('darken');
+const closeBtn = document.querySelector('.closebtn');
+let formOpened = false;
+
+function toggleForm() {
+  if (formOpened === false) {
+    bookForm.style.display = 'flex';
+    darken.style.filter = 'brightness(70%)';
+  } else {
+    bookForm.style.display = 'none';
+    darken.style.filter = null;
+  }
+  formOpened = !formOpened;
+}
+
+addBookBtn.addEventListener('click', toggleForm);
+closeBtn.addEventListener('click', toggleForm);
